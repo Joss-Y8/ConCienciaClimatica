50f3f1fcbbdc7d74bbc475b464f2476e80a21d23
Joss-Y8 <romero080505@gmail.com>
2025-05-24 20:12:32 -0600

    modificación de clase Usuario

--- concienciaclimatica/backend/api/USUARIO/Usuario.php
+++ concienciaclimatica/backend/api/USUARIO/Usuario.php
@@ -1,7 +1,7 @@
 <?php
-namespace CLIMATICA\API\LOGIN;
+namespace CLIMATICA\API\USUARIO;
 
-require_once __DIR__ . '/../Database.php';
+require_once __DIR__ . '/../DataBase.php';
 
 use CLIMATICA\API\DataBase;
 
@@ -28,6 +28,7 @@ class Usuario extends DataBase {
                 $usuario = $result->fetch_assoc();
 
                 if (password_verify($jsonOBJ->password, $usuario['password'])) {
+                    $_SESSION['id_usuario'] = $usuario['id']; 
                     $this->data = [
                         'status' => 'success',
                         'message' => 'Inicio de sesión exitoso',
@@ -83,6 +84,50 @@ class Usuario extends DataBase {
         }
 
         return $this->data;
+    }
+
+    public function logout(){
+        session_destroy(); 
+        return['status' => 'succes', 'message' => 'Tu sesión ha sido cerrada correctamente. ¡Vuelve pronto']; 
+    }
+
+    //Utilizamos la función para obtener los datos del perfil
+    public function obtenerPerfil($idUsuario){
+        $sql = "SELECT id, nombre, apellido, correo, edad, zona FROM usuarios WHERE id=?"; 
+        $stmt = $this->conexion->prepare($sql); 
+        $stmt->bind_param("i", $idUsuario); 
+        $stmt->execute(); 
+        $result = $stmt->get_result(); 
+
+        if($result->num_rows === 1){
+            $this->data = $result->fetch_assoc(); 
+        }else {
+            $this->data = ['status' => 'error', 'messagge'=> 'Usuario no encontrado']; 
+        }
+
+        $stmt->close(); 
+        return $this->data; 
+    }
+
+    public function actualizarPerfil($idUsuario, $jsonOBJ){
+        $nombre   = $this->conexion->real_escape_string($jsonOBJ->nombre ?? '');
+        $apellido = $this->conexion->real_escape_string($jsonOBJ->apellido ?? '');
+        $correo   = $this->conexion->real_escape_string($jsonOBJ->correo ?? '');
+        $edad     = intval($jsonOBJ->edad ?? 0);
+        $zona     = $this->conexion->real_escape_string($jsonOBJ->zona ?? '');
+
+        $sql = "UPDATE usuarios SET nombre = ?, apellido = ?, correo = ?, edad = ?, zona = ? WHERE id= ?"; 
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->bind_param("sssisi", $nombre, $apellido, $correo, $edad, $zona, $idUsuario); 
+
+        if($stmt->execute()){
+            $this->data = ['status' => 'success', 'message' => 'Perfil actualizado correctamente']; 
+        } else {
+            $this->data = ['status' => 'error', 'message' => 'Error al actualizar']; 
+        }
+
+        $stmt->close(); 
+        return $this->data; 
     }
 }
 ?>
